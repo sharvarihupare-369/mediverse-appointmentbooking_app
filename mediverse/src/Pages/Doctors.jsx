eb04e6401e0fadd59384bd6783b8667cf1f84890
sharvarihupare-369 <sharvarihupare15@gmail.com>
2023-05-05 23:03:46 +0530

    day-3

--- mediverse/src/Pages/Doctors.jsx
+++ mediverse/src/Pages/Doctors.jsx
@@ -1,9 +1,101 @@
-import React from 'react'
+import { Box, Button, Flex, Heading, Image, Text } from '@chakra-ui/react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 
 const Doctors = () => {
-  return (
-    <div>Doctors</div>
-  )
-}
+  const [loading, setLoading] = useState(false);
+  const [data, setData] = useState([]);
+  const [Error, setError] = useState(false);
+  const [page, setPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(0);
 
-export default Doctors
+  const fetchDoctorData = page => {
+    setLoading(true);
+    axios
+      .get(`http://localhost:3000/doctors?_page=${page}&_limit=12`)
+      .then(res => {
+        setData(res.data);
+        let pages = res.headers['x-total-count'];
+        setTotalPages(Math.ceil(pages / 12));
+        setLoading(false);
+      })
+      .catch(() => {
+        setError(true);
+        setLoading(false);
+      });
+  };
+
+  useEffect(() => {
+    fetchDoctorData(page);
+  }, [page]);
+
+  return (
+    <>
+      <Box
+        w="70%"
+        m="auto"
+        gap="40px"
+        display={'grid'}
+        gridTemplateColumns={'repeat(3,1fr)'}
+        textAlign={'center'}
+        p="10px"
+      >
+        {data.map((doctor, ind) => {
+          console.log(doctor);
+          return (
+            <Box boxShadow="rgba(0, 0, 0, 0.24) 0px 3px 8px">
+              <Image width={'400px'} m="auto" src={doctor.image} />
+              <Heading as="h5" size="sm">
+                {doctor.name}
+              </Heading>
+              <Text>Specialist : {doctor.specialist}</Text>
+              <Text>Exp : {doctor.experience}</Text>
+              <Text></Text>
+              <Button>Book Appointment</Button>
+            </Box>
+          );
+        })}
+      </Box>
+      <Box m="5px 5px" alignItems={"center"} display={'flex'} justifyContent={'center'} ga="5px">
+        <Button
+          color={'white'}
+          bg={'#222566'}
+          _hover={{
+            bg: '#3879E9',
+          }}
+          disabled={page == 1}
+          onClick={() => setPage(page - 1)}
+        >
+          PREV
+        </Button>
+        {/* <Button>{page}</Button> */}
+        {new Array(totalPages).fill(0).map((el, ind) => {
+          return (
+            <Button
+               m="5px"
+              _hover={{
+                bg: '#3879E9',
+              }}
+              onClick={() => setPage(page + 1)}
+            >
+              {ind + 1}
+            </Button>
+          );
+        })}
+        <Button
+          color={'white'}
+          bg={'#222566'}
+          _hover={{
+            bg: '#3879E9',
+          }}
+          disabled={page === totalPages}
+          onClick={() => setPage(page + 1)}
+        >
+          NEXT
+        </Button>
+      </Box>
+    </>
+  );
+};
+
+export default Doctors;
--- mediverse/src/Pages/Home.jsx
+++ mediverse/src/Pages/Home.jsx
@@ -6,24 +6,32 @@ import {
   Heading,
   Image,
   Text,
+  Flex,
 } from '@chakra-ui/react';
 import React from 'react';
 import 'animate.css';
-import imag1 from '../Assets/vectorImage2.jpg'
-import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import imag1 from '../Assets/vectorImage2.jpg';
+import 'react-responsive-carousel/lib/styles/carousel.min.css'; // requires a loader
 import { Carousel } from 'react-responsive-carousel';
 import { Form } from 'react-router-dom';
 import SliderPro from '../components/SliderPro';
+import BookAppointForm from '../components/BookAppointForm';
 
 const Home = () => {
   return (
     <>
-     
       <Box
         bgImage="url('https://img.freepik.com/free-photo/young-professional-doctor-woman-physician-with-stethoscope-holding-digital-tablet-smiling-cam_1258-127422.jpg?w=1380&t=st=1683121920~exp=1683122520~hmac=8191b5cca491733f4d7775b9a4d3c420f30e2c74a3d9c03f29202bb91ba61e53')"
         //   backgroundPosition=""
         backgroundRepeat="no-repeat"
-        backgroundSize={{base:"cover",sm:"cover",md:"cover",lg:"cover",xl:"cover","2xl":"cover"}}
+        backgroundSize={{
+          base: 'cover',
+          sm: 'cover',
+          md: 'cover',
+          lg: 'cover',
+          xl: 'cover',
+          '2xl': 'cover',
+        }}
         opacity={'0.8'}
       >
         <Container
@@ -60,24 +68,98 @@ const Home = () => {
         </Container>
       </Box>
 
+      <Box position={'relative'} bottom={'30px'}>
+        <Box>
+          <BookAppointForm />
+        </Box>
+      </Box>
+      <Heading
+          fontWeight={'500'}
+          color={'#222566'}
+          as="h1"
+          size="xl"
+          textAlign={'center'}
+        >    Our Excellence Centers
+        </Heading>
+      <Flex m="60px"   justifyContent={"space-around"}>
+        
+      
+        <Box w="200px" textAlign={'center'}>
+          <Image
+            borderRadius={'50%'}
+            src="https://img.freepik.com/premium-vector/medical-stethoscope-with-human-heart-health-care-medicine_284092-2696.jpg?size=626&ext=jpg&ga=GA1.2.205266656.1682997817&semt=ais"
+          />
+          <Heading size="sm" as="h4">
+            Cardiology
+          </Heading>
+        </Box>
+        <Box w="200px" textAlign={'center'}>
+          <Image
+            borderRadius={'50%'}
+            src="https://img.freepik.com/free-vector/human-nervous-system_53876-90443.jpg?size=626&ext=jpg&ga=GA1.1.205266656.1682997817&semt=ais"
+          />
+            <Heading mt='5px' size="sm" as="h4">
+            Neurology
+          </Heading>
+        </Box>
+        <Box w="220px" textAlign={'center'}>
+          <Image
+            borderRadius={'50%'}
+            src="https://img.freepik.com/free-vector/human-internal-organ-with-stomach_1308-108170.jpg?size=626&ext=jpg&ga=GA1.1.205266656.1682997817&semt=ais"
+          />
+          <Heading mt='5px' size="sm" as="h4">
+            GastronEnterology
+          </Heading>
+        </Box>
+        <Box w="230px" textAlign={'center'}>
+          <Image
+            borderRadius={'50%'}
+            src="https://img.freepik.com/free-vector/stethoscope-lungs-earth-globe_1308-126019.jpg?size=626&ext=jpg&ga=GA1.1.205266656.1682997817&semt=ais"
+          />
+          <Heading size="sm" as="h4">
+            Pulmonology
+          </Heading>
+        </Box>
+        <Box w="200px" textAlign={'center'}>
+          <Image
+            borderRadius={'50%'}
+            src="https://img.freepik.com/free-vector/anatomy-human-pelvis-white-background_1308-92151.jpg?size=626&ext=jpg&ga=GA1.1.205266656.1682997817&semt=ais"
+          />
+          <Heading size="sm" as="h4">
+            Orthopedics
+          </Heading>
+        </Box>
 
-      <Box bgGradient='linear(to-r, #3879E9, #222566)'>
+      </Flex>
+
+      <Box bgGradient="linear(to-r, #3879E9, #222566)">
         {/* <Image w="100%" src='https://img.freepik.com/free-vector/hospital-medicine-infographic-with-characters-physicians-working-hospital-caring-patients-fighting-viruses_1284-54255.jpg?w=1380&t=st=1683130927~exp=1683131527~hmac=4dc6c753903d6f170c654569ed18c17c652a2ad8d6dd2473821e3d63c8044fb6' /> */}
 
-     {/* <Container> */}
-        <Box p="100px"  className="animate__animated animate__fadeInLeftBig" textAlign={"center"} alignItems={"center"} color={"white"}>
-            <Heading >Welcome to the smart way of digitally managing health</Heading>
-            </Box>
-     {/* </Container> */}
+        {/* <Container> */}
+        <Box
+          p="100px"
+          className="animate__animated animate__fadeInLeftBig"
+          textAlign={'center'}
+          alignItems={'center'}
+          color={'white'}
+        >
+          <Heading>
+            Welcome to the smart way of digitally managing health
+          </Heading>
+        </Box>
+        {/* </Container> */}
       </Box>
-      <SliderPro/>
+      <SliderPro />
 
-      <Box w="100%" >
-        <Image className="animate__animated  animate__backInUp" w="100%" src={imag1} />
+      <Box w="100%">
+        <Image
+          className="animate__animated  animate__backInUp"
+          w="100%"
+          src={imag1}
+        />
       </Box>
     </>
   );
 };
 
 export default Home;
-
